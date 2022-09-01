50de21d27577e2030ea938cb93b9b346129320bb
jobaer-ahmed <ajobaer08@gmail.com>
2022-09-01 16:37:29 +0800

    added dynamic api

--- js/meals.js
+++ js/meals.js
@@ -1,5 +1,5 @@
-const loadMeal = () => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=fish`
+const loadMeal = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
@@ -7,12 +7,12 @@ const loadMeal = () => {
 
 const displayMeals = meals => {
     const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.innerHTML = ``;
     meals.forEach(meal => {
-        console.log(meal);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-            <div class="card">
+            <div onclick="displayMealDetail(${meal.idMeal})" class="card">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Name: ${meal.strMeal}</h5>
@@ -24,4 +24,35 @@ const displayMeals = meals => {
     })
 }
 
-loadMeal();
+document.getElementById('search-btn').addEventListener('click', function () {
+    const inputElement = document.getElementById('food-name');
+    const foodName = inputElement.value;
+    loadMeal(foodName);
+    inputElement.value = '';
+})
+
+const displayMealDetail = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeal(data.meals[0]))
+}
+
+const displayMeal = meal => {
+
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = ``;
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = `
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                    the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `;
+    detailContainer.appendChild(mealDiv);
+}
